eda5d943fc70f5e3691460014e65bf958d03a4ec
vandanabadal <pinchofswad@gmail.com>
2020-04-01 17:35:54 -0400

    added search page and modified login code

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -1,5 +1,12 @@
 package com.qa.pages;
 
 public class LoginPage {
+	
+	public void loginPage() {
+		System.out.println("Login Page");
+	}
 
+	public void loginSet() {
+		System.out.println("Login Set");
+	}
 }
