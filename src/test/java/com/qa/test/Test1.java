a3a39cb883dcd6d0c5ac3086e3845e7489f94453
vandanabadal <pinchofswad@gmail.com>
2020-04-02 18:32:21 -0400

    changed src-test-test1

--- src/test/java/com/qa/test/Test1.java
+++ src/test/java/com/qa/test/Test1.java
@@ -1,5 +1,26 @@
 package com.qa.test;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class Test1 {
+	
+	@BeforeMethod
+	public void test2() {
+		System.out.println("Before Method");
+		
+	}
+	
+	@Test
+	public void test1() {
+		System.out.println("test 1 ");
+	}
+	
+	@AfterMethod
+	public void test3() {
+		System.out.println("After Method");
+		
+	}
 
 }
